99aaa7e393a4f39333057e6a618ce0d33d55dd55
Yaman-Goyal <yaman.goyal@quadbtech.com>
2024-05-07 15:17:39 +0530

    Changes in Swap Modal (More Responsive)

--- src/icp_dex_frontend/src/Modals/Dialouge.jsx
+++ src/icp_dex_frontend/src/Modals/Dialouge.jsx
@@ -1,8 +1,12 @@
+import { Dot } from 'lucide-react';
 const DialogBox = ({ text }) => {
     return (
-        <div className=" z-50 inset-0 flex justify-center items-center mx-auto ">
-            <div className="bg-[#010427] text-white p-4 rounded-lg shadow-xl font-cabin text-lg font-medium">
-                <p>{text}</p>
+        <div className=" z-50 inset-0 flex justify-center items-center">
+            <div className="bg-[#010427] text-white p-2 sm:p-4 rounded-lg shadow-xl font-cabin custom-text-size-14 sm:text-lg font-medium flex justify-center ">
+                <span>
+                    <Dot color='#F7931A' />
+                </span>
+                <span>{text}</span>
             </div>
         </div>
     );
